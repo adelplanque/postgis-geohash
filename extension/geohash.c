670662ebe425a6727a183c220a623097f3c69bf8
adelplanque <adelplanque@users.noreply.github.com>
2021-08-07 09:16:05 +0200

    Build liblwgeom, and link to.

--- extension/geohash.c
+++ extension/geohash.c
@@ -1,6 +1,27 @@
+/**********************************************************************
+ *
+ * Postgis-Geohash
+ * Copyright 2017, 2021 Alain Delplanque
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ *
+ **********************************************************************/
+
+#include <math.h>
 #include <postgres.h>
 #include <fmgr.h>
-#include <liblwgeom.h>
+#include "../ext/postgis/liblwgeom/liblwgeom.h"
 
 #define PG_GETARG_GSERIALIZED_P(varno) ((GSERIALIZED *)PG_DETOAST_DATUM(PG_GETARG_DATUM(varno)))
 
